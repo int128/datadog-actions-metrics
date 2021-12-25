ef0db1f1731f5d3804fd5df62b9e5737a01eccbb
Hidetake Iwata <int128@gmail.com>
2021-12-25 19:07:54 +0900

    Add first commit related metrics

---
+++ src/generated/graphql.ts
@@ -0,0 +1,10 @@
+import * as Types from './graphql-types';
+
+export type ClosedPullRequestQueryVariables = Types.Exact<{
+  owner: Types.Scalars['String'];
+  name: Types.Scalars['String'];
+  number: Types.Scalars['Int'];
+}>;
+
+
+export type ClosedPullRequestQuery = { __typename?: 'Query', repository?: { __typename?: 'Repository', pullRequest?: { __typename?: 'PullRequest', commits: { __typename?: 'PullRequestCommitConnection', nodes?: Array<{ __typename?: 'PullRequestCommit', commit: { __typename?: 'Commit', authoredDate: any, committedDate: any } } | null | undefined> | null | undefined } } | null | undefined } | null | undefined };
