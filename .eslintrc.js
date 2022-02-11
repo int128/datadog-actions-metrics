cef4a4f181b4ad688b491d4836dcc7b01d09d94e
Hidetake Iwata <int128@gmail.com>
2022-02-11 16:40:21 +0900

    Update .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+// https://typescript-eslint.io/docs/linting/
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint', 'jest'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+}
