e7e377545f88c92543dfe626ef6cf2ed14324cde
Hidetake Iwata <int128@gmail.com>
2022-07-02 19:16:36 +0900

    Fix API usage

--- src/schedule/metrics.ts
+++ src/schedule/metrics.ts
@@ -1,11 +1,11 @@
-import { Series } from '@datadog/datadog-api-client/dist/packages/datadog-api-client-v1/models/Series'
+import { v1 } from '@datadog/datadog-api-client'
 import { GitHubContext, ListWorkflowRunsForRepoRateLimitResponse } from '../types'
 
 export const computeScheduleMetrics = (
   context: GitHubContext,
   queuedWorkflowRuns: ListWorkflowRunsForRepoRateLimitResponse,
   now: Date
-): Series[] => {
+): v1.Series[] => {
   const tags = [`repository_owner:${context.repo.owner}`, `repository_name:${context.repo.repo}`]
   const t = now.getTime() / 1000
   return [
