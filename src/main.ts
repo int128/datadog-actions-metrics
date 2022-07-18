5a20a65672c0596ab4b08262e2d939c8b9cbc35f
Hidetake Iwata <int128@gmail.com>
2022-07-18 12:37:46 +0900

    Refactor main (#395)

--- src/main.ts
+++ src/main.ts
@@ -13,4 +13,4 @@ const main = async (): Promise<void> => {
   })
 }
 
-main().catch((e) => core.setFailed(e instanceof Error ? e.message : String(e)))
+main().catch((e) => core.setFailed(e instanceof Error ? e : String(e)))
