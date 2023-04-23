2834cd37aebf1a8f8dbfff0c89dbc913526d979f
Hidetake Iwata <int128@gmail.com>
2023-04-23 08:58:25 +0000

    Fix type error

--- tests/fixtures.ts
+++ tests/fixtures.ts
@@ -1,5 +1,10 @@
 import WebhookDefinitions, { WebhookDefinition } from '@octokit/webhooks-examples'
-import { PullRequestEvent, WebhookEventName, WorkflowRunEvent } from '@octokit/webhooks-types'
+import {
+  PullRequestClosedEvent,
+  PullRequestOpenedEvent,
+  WebhookEventName,
+  WorkflowRunCompletedEvent,
+} from '@octokit/webhooks-types'
 
 const getExamplesByEventName = <E extends WebhookEventName>(eventName: E) => {
   for (const definition of WebhookDefinitions) {
@@ -15,30 +20,29 @@ const isTypeOfEventName = <TName extends WebhookEventName>(
   eventName: TName
 ): definition is WebhookDefinition<TName> => definition.name === eventName
 
-const isTypeOfAction = <TEvent extends { action: string }, TAction extends TEvent['action']>(
-  event: TEvent,
-  action: TAction
-): event is TEvent & { action: TAction } => event.action === action
-
-const pullRequestExampleOfAction = <TAction extends PullRequestEvent['action']>(action: TAction) => {
+export const examplePullRequestClosedEvent: PullRequestClosedEvent = (() => {
   for (const example of getExamplesByEventName('pull_request')) {
-    if (isTypeOfAction(example, action)) {
+    if (example.action === 'closed') {
       return example
     }
   }
-  throw new Error(`definition of action ${action} not found`)
-}
+  throw new Error(`unexpected type error`)
+})()
 
-export const examplePullRequestClosedEvent = pullRequestExampleOfAction('closed')
-export const examplePullRequestOpenedEvent = pullRequestExampleOfAction('opened')
+export const examplePullRequestOpenedEvent: PullRequestOpenedEvent = (() => {
+  for (const example of getExamplesByEventName('pull_request')) {
+    if (example.action === 'opened') {
+      return example
+    }
+  }
+  throw new Error(`unexpected type error`)
+})()
 
-const workflowRunExampleOfAction = <T extends WorkflowRunEvent['action']>(action: T) => {
+export const exampleWorkflowRunCompletedEvent: WorkflowRunCompletedEvent = (() => {
   for (const example of getExamplesByEventName('workflow_run')) {
-    if (isTypeOfAction(example, action)) {
+    if (example.action === 'completed') {
       return example
     }
   }
-  throw new Error(`definition of action ${action} not found`)
-}
-
-export const exampleWorkflowRunCompletedEvent = workflowRunExampleOfAction('completed')
+  throw new Error(`unexpected type error`)
+})()
