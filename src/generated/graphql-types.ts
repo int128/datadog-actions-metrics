3c1879fef0ffba9b8a435b58405b011c92dd6b47
update-generated-files-action <41898282+github-actions[bot]@users.noreply.github.com>
2023-06-08 01:44:38 +0000

    Generated by GitHub Actions (ts / generate)
    https://github.com/int128/datadog-actions-metrics/actions/runs/5206471074

--- src/generated/graphql-types.ts
+++ src/generated/graphql-types.ts
@@ -8043,7 +8043,7 @@ export enum IpAllowListForInstalledAppsEnabledSettingValue {
 export type IpAllowListOwner = App | Enterprise | Organization;
 
 /** An Issue is a place to discuss ideas, enhancements, tasks, and bugs for a project. */
-export type Issue = Assignable & Closable & Comment & Labelable & Lockable & Node & ProjectV2Owner & Reactable & RepositoryNode & Subscribable & UniformResourceLocatable & Updatable & UpdatableComment & {
+export type Issue = Assignable & Closable & Comment & Deletable & Labelable & Lockable & Node & ProjectV2Owner & Reactable & RepositoryNode & Subscribable & UniformResourceLocatable & Updatable & UpdatableComment & {
   __typename?: 'Issue';
   /** Reason that the conversation was locked. */
   activeLockReason?: Maybe<LockReason>;
@@ -8149,6 +8149,8 @@ export type Issue = Assignable & Closable & Comment & Labelable & Lockable & Nod
   userContentEdits?: Maybe<UserContentEditConnection>;
   /** Indicates if the object can be closed by the viewer. */
   viewerCanClose: Scalars['Boolean']['output'];
+  /** Check if the current viewer can delete this object. */
+  viewerCanDelete: Scalars['Boolean']['output'];
   /** Can user react to this subject */
   viewerCanReact: Scalars['Boolean']['output'];
   /** Indicates if the object can be reopened by the viewer. */
@@ -14114,10 +14116,14 @@ export type OrganizationMigration = Node & {
 export enum OrganizationMigrationState {
   /** The Octoshift migration has failed. */
   Failed = 'FAILED',
+  /** The Octoshift migration has invalid credentials. */
+  FailedValidation = 'FAILED_VALIDATION',
   /** The Octoshift migration is in progress. */
   InProgress = 'IN_PROGRESS',
   /** The Octoshift migration has not started. */
   NotStarted = 'NOT_STARTED',
+  /** The Octoshift migration needs to have its credentials validated. */
+  PendingValidation = 'PENDING_VALIDATION',
   /** The Octoshift migration is performing post repository migrations. */
   PostRepoMigration = 'POST_REPO_MIGRATION',
   /** The Octoshift migration is performing pre repository migrations. */
