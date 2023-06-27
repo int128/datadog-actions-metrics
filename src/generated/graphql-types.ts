4d12ec2586bdb856292421c26d1be7eb1ca8e812
update-generated-files-action <41898282+github-actions[bot]@users.noreply.github.com>
2023-06-27 19:43:19 +0000

    Generated by GitHub Actions (ts / generate)
    https://github.com/int128/datadog-actions-metrics/actions/runs/5393950685

--- src/generated/graphql-types.ts
+++ src/generated/graphql-types.ts
@@ -12717,6 +12717,52 @@ export type OrgOauthAppAccessApprovedAuditEntry = AuditEntry & Node & OauthAppli
   userUrl?: Maybe<Scalars['URI']['output']>;
 };
 
+/** Audit log entry for a org.oauth_app_access_blocked event. */
+export type OrgOauthAppAccessBlockedAuditEntry = AuditEntry & Node & OauthApplicationAuditEntryData & OrganizationAuditEntryData & {
+  __typename?: 'OrgOauthAppAccessBlockedAuditEntry';
+  /** The action name */
+  action: Scalars['String']['output'];
+  /** The user who initiated the action */
+  actor?: Maybe<AuditEntryActor>;
+  /** The IP address of the actor */
+  actorIp?: Maybe<Scalars['String']['output']>;
+  /** A readable representation of the actor's location */
+  actorLocation?: Maybe<ActorLocation>;
+  /** The username of the user who initiated the action */
+  actorLogin?: Maybe<Scalars['String']['output']>;
+  /** The HTTP path for the actor. */
+  actorResourcePath?: Maybe<Scalars['URI']['output']>;
+  /** The HTTP URL for the actor. */
+  actorUrl?: Maybe<Scalars['URI']['output']>;
+  /** The time the action was initiated */
+  createdAt: Scalars['PreciseDateTime']['output'];
+  id: Scalars['ID']['output'];
+  /** The name of the OAuth application. */
+  oauthApplicationName?: Maybe<Scalars['String']['output']>;
+  /** The HTTP path for the OAuth application */
+  oauthApplicationResourcePath?: Maybe<Scalars['URI']['output']>;
+  /** The HTTP URL for the OAuth application */
+  oauthApplicationUrl?: Maybe<Scalars['URI']['output']>;
+  /** The corresponding operation type for the action */
+  operationType?: Maybe<OperationType>;
+  /** The Organization associated with the Audit Entry. */
+  organization?: Maybe<Organization>;
+  /** The name of the Organization. */
+  organizationName?: Maybe<Scalars['String']['output']>;
+  /** The HTTP path for the organization */
+  organizationResourcePath?: Maybe<Scalars['URI']['output']>;
+  /** The HTTP URL for the organization */
+  organizationUrl?: Maybe<Scalars['URI']['output']>;
+  /** The user affected by the action */
+  user?: Maybe<User>;
+  /** For actions involving two users, the actor is the initiator and the user is the affected user. */
+  userLogin?: Maybe<Scalars['String']['output']>;
+  /** The HTTP path for the user. */
+  userResourcePath?: Maybe<Scalars['URI']['output']>;
+  /** The HTTP URL for the user. */
+  userUrl?: Maybe<Scalars['URI']['output']>;
+};
+
 /** Audit log entry for a org.oauth_app_access_denied event. */
 export type OrgOauthAppAccessDeniedAuditEntry = AuditEntry & Node & OauthApplicationAuditEntryData & OrganizationAuditEntryData & {
   __typename?: 'OrgOauthAppAccessDeniedAuditEntry';
@@ -12766,6 +12812,52 @@ export type OrgOauthAppAccessDeniedAuditEntry = AuditEntry & Node & OauthApplica
 /** Audit log entry for a org.oauth_app_access_requested event. */
 export type OrgOauthAppAccessRequestedAuditEntry = AuditEntry & Node & OauthApplicationAuditEntryData & OrganizationAuditEntryData & {
   __typename?: 'OrgOauthAppAccessRequestedAuditEntry';
+  /** The action name */
+  action: Scalars['String']['output'];
+  /** The user who initiated the action */
+  actor?: Maybe<AuditEntryActor>;
+  /** The IP address of the actor */
+  actorIp?: Maybe<Scalars['String']['output']>;
+  /** A readable representation of the actor's location */
+  actorLocation?: Maybe<ActorLocation>;
+  /** The username of the user who initiated the action */
+  actorLogin?: Maybe<Scalars['String']['output']>;
+  /** The HTTP path for the actor. */
+  actorResourcePath?: Maybe<Scalars['URI']['output']>;
+  /** The HTTP URL for the actor. */
+  actorUrl?: Maybe<Scalars['URI']['output']>;
+  /** The time the action was initiated */
+  createdAt: Scalars['PreciseDateTime']['output'];
+  id: Scalars['ID']['output'];
+  /** The name of the OAuth application. */
+  oauthApplicationName?: Maybe<Scalars['String']['output']>;
+  /** The HTTP path for the OAuth application */
+  oauthApplicationResourcePath?: Maybe<Scalars['URI']['output']>;
+  /** The HTTP URL for the OAuth application */
+  oauthApplicationUrl?: Maybe<Scalars['URI']['output']>;
+  /** The corresponding operation type for the action */
+  operationType?: Maybe<OperationType>;
+  /** The Organization associated with the Audit Entry. */
+  organization?: Maybe<Organization>;
+  /** The name of the Organization. */
+  organizationName?: Maybe<Scalars['String']['output']>;
+  /** The HTTP path for the organization */
+  organizationResourcePath?: Maybe<Scalars['URI']['output']>;
+  /** The HTTP URL for the organization */
+  organizationUrl?: Maybe<Scalars['URI']['output']>;
+  /** The user affected by the action */
+  user?: Maybe<User>;
+  /** For actions involving two users, the actor is the initiator and the user is the affected user. */
+  userLogin?: Maybe<Scalars['String']['output']>;
+  /** The HTTP path for the user. */
+  userResourcePath?: Maybe<Scalars['URI']['output']>;
+  /** The HTTP URL for the user. */
+  userUrl?: Maybe<Scalars['URI']['output']>;
+};
+
+/** Audit log entry for a org.oauth_app_access_unblocked event. */
+export type OrgOauthAppAccessUnblockedAuditEntry = AuditEntry & Node & OauthApplicationAuditEntryData & OrganizationAuditEntryData & {
+  __typename?: 'OrgOauthAppAccessUnblockedAuditEntry';
   /** The action name */
   action: Scalars['String']['output'];
   /** The user who initiated the action */
@@ -13897,7 +13989,7 @@ export type OrganizationTotalSponsorshipAmountAsSponsorInCentsArgs = {
 };
 
 /** An audit entry in an organization audit log. */
-export type OrganizationAuditEntry = MembersCanDeleteReposClearAuditEntry | MembersCanDeleteReposDisableAuditEntry | MembersCanDeleteReposEnableAuditEntry | OauthApplicationCreateAuditEntry | OrgAddBillingManagerAuditEntry | OrgAddMemberAuditEntry | OrgBlockUserAuditEntry | OrgConfigDisableCollaboratorsOnlyAuditEntry | OrgConfigEnableCollaboratorsOnlyAuditEntry | OrgCreateAuditEntry | OrgDisableOauthAppRestrictionsAuditEntry | OrgDisableSamlAuditEntry | OrgDisableTwoFactorRequirementAuditEntry | OrgEnableOauthAppRestrictionsAuditEntry | OrgEnableSamlAuditEntry | OrgEnableTwoFactorRequirementAuditEntry | OrgInviteMemberAuditEntry | OrgInviteToBusinessAuditEntry | OrgOauthAppAccessApprovedAuditEntry | OrgOauthAppAccessDeniedAuditEntry | OrgOauthAppAccessRequestedAuditEntry | OrgRemoveBillingManagerAuditEntry | OrgRemoveMemberAuditEntry | OrgRemoveOutsideCollaboratorAuditEntry | OrgRestoreMemberAuditEntry | OrgUnblockUserAuditEntry | OrgUpdateDefaultRepositoryPermissionAuditEntry | OrgUpdateMemberAuditEntry | OrgUpdateMemberRepositoryCreationPermissionAuditEntry | OrgUpdateMemberRepositoryInvitationPermissionAuditEntry | PrivateRepositoryForkingDisableAuditEntry | PrivateRepositoryForkingEnableAuditEntry | RepoAccessAuditEntry | RepoAddMemberAuditEntry | RepoAddTopicAuditEntry | RepoArchivedAuditEntry | RepoChangeMergeSettingAuditEntry | RepoConfigDisableAnonymousGitAccessAuditEntry | RepoConfigDisableCollaboratorsOnlyAuditEntry | RepoConfigDisableContributorsOnlyAuditEntry | RepoConfigDisableSockpuppetDisallowedAuditEntry | RepoConfigEnableAnonymousGitAccessAuditEntry | RepoConfigEnableCollaboratorsOnlyAuditEntry | RepoConfigEnableContributorsOnlyAuditEntry | RepoConfigEnableSockpuppetDisallowedAuditEntry | RepoConfigLockAnonymousGitAccessAuditEntry | RepoConfigUnlockAnonymousGitAccessAuditEntry | RepoCreateAuditEntry | RepoDestroyAuditEntry | RepoRemoveMemberAuditEntry | RepoRemoveTopicAuditEntry | RepositoryVisibilityChangeDisableAuditEntry | RepositoryVisibilityChangeEnableAuditEntry | TeamAddMemberAuditEntry | TeamAddRepositoryAuditEntry | TeamChangeParentTeamAuditEntry | TeamRemoveMemberAuditEntry | TeamRemoveRepositoryAuditEntry;
+export type OrganizationAuditEntry = MembersCanDeleteReposClearAuditEntry | MembersCanDeleteReposDisableAuditEntry | MembersCanDeleteReposEnableAuditEntry | OauthApplicationCreateAuditEntry | OrgAddBillingManagerAuditEntry | OrgAddMemberAuditEntry | OrgBlockUserAuditEntry | OrgConfigDisableCollaboratorsOnlyAuditEntry | OrgConfigEnableCollaboratorsOnlyAuditEntry | OrgCreateAuditEntry | OrgDisableOauthAppRestrictionsAuditEntry | OrgDisableSamlAuditEntry | OrgDisableTwoFactorRequirementAuditEntry | OrgEnableOauthAppRestrictionsAuditEntry | OrgEnableSamlAuditEntry | OrgEnableTwoFactorRequirementAuditEntry | OrgInviteMemberAuditEntry | OrgInviteToBusinessAuditEntry | OrgOauthAppAccessApprovedAuditEntry | OrgOauthAppAccessBlockedAuditEntry | OrgOauthAppAccessDeniedAuditEntry | OrgOauthAppAccessRequestedAuditEntry | OrgOauthAppAccessUnblockedAuditEntry | OrgRemoveBillingManagerAuditEntry | OrgRemoveMemberAuditEntry | OrgRemoveOutsideCollaboratorAuditEntry | OrgRestoreMemberAuditEntry | OrgUnblockUserAuditEntry | OrgUpdateDefaultRepositoryPermissionAuditEntry | OrgUpdateMemberAuditEntry | OrgUpdateMemberRepositoryCreationPermissionAuditEntry | OrgUpdateMemberRepositoryInvitationPermissionAuditEntry | PrivateRepositoryForkingDisableAuditEntry | PrivateRepositoryForkingEnableAuditEntry | RepoAccessAuditEntry | RepoAddMemberAuditEntry | RepoAddTopicAuditEntry | RepoArchivedAuditEntry | RepoChangeMergeSettingAuditEntry | RepoConfigDisableAnonymousGitAccessAuditEntry | RepoConfigDisableCollaboratorsOnlyAuditEntry | RepoConfigDisableContributorsOnlyAuditEntry | RepoConfigDisableSockpuppetDisallowedAuditEntry | RepoConfigEnableAnonymousGitAccessAuditEntry | RepoConfigEnableCollaboratorsOnlyAuditEntry | RepoConfigEnableContributorsOnlyAuditEntry | RepoConfigEnableSockpuppetDisallowedAuditEntry | RepoConfigLockAnonymousGitAccessAuditEntry | RepoConfigUnlockAnonymousGitAccessAuditEntry | RepoCreateAuditEntry | RepoDestroyAuditEntry | RepoRemoveMemberAuditEntry | RepoRemoveTopicAuditEntry | RepositoryVisibilityChangeDisableAuditEntry | RepositoryVisibilityChangeEnableAuditEntry | TeamAddMemberAuditEntry | TeamAddRepositoryAuditEntry | TeamChangeParentTeamAuditEntry | TeamRemoveMemberAuditEntry | TeamRemoveRepositoryAuditEntry;
 
 /** The connection type for OrganizationAuditEntry. */
 export type OrganizationAuditEntryConnection = {
@@ -17602,6 +17694,8 @@ export type PullRequestThread = Node & {
   isResolved: Scalars['Boolean']['output'];
   /** The line in the file to which this thread refers */
   line?: Maybe<Scalars['Int']['output']>;
+  /** Identifies the file path of this thread. */
+  path: Scalars['String']['output'];
   /** Identifies the pull request associated with this thread. */
   pullRequest: PullRequest;
   /** Identifies the repository associated with this thread. */
@@ -17612,6 +17706,8 @@ export type PullRequestThread = Node & {
   startDiffSide?: Maybe<DiffSide>;
   /** The line of the first file diff in the thread. */
   startLine?: Maybe<Scalars['Int']['output']>;
+  /** The level at which the comments in the corresponding thread are targeted, can be a diff line or a file */
+  subjectType: PullRequestReviewThreadSubjectType;
   /** Indicates whether the current viewer can reply to this thread. */
   viewerCanReply: Scalars['Boolean']['output'];
   /** Whether or not the viewer can resolve this thread */
