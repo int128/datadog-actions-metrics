f6e1bd4e75d239f8fe6279ed3fa7dd4532acd9dc
update-generated-files-action <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-14 20:00:45 +0000

    Generated by GitHub Actions (ts / generate)
    https://github.com/int128/datadog-actions-metrics/actions/runs/7906696706

--- src/generated/graphql-types.ts
+++ src/generated/graphql-types.ts
@@ -7729,6 +7729,8 @@ export enum FundingPlatform {
   Otechie = 'OTECHIE',
   /** Patreon funding platform. */
   Patreon = 'PATREON',
+  /** Polar funding platform. */
+  Polar = 'POLAR',
   /** Tidelift funding platform. */
   Tidelift = 'TIDELIFT'
 }
