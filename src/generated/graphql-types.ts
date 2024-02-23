3d24dc873cfe06f1e997fffcf26c0d0436ded9e7
update-generated-files-action <41898282+github-actions[bot]@users.noreply.github.com>
2024-02-23 19:22:45 +0000

    Generated by GitHub Actions (ts / generate)
    https://github.com/int128/datadog-actions-metrics/actions/runs/8024050730

--- src/generated/graphql-types.ts
+++ src/generated/graphql-types.ts
@@ -10212,7 +10212,7 @@ export type Migration = {
   sourceUrl: Scalars['URI']['output'];
   /** The migration state. */
   state: MigrationState;
-  /** The number of warnings encountered for this migration. To review the warnings, check the [Migration Log](https://docs.github.com/en/migrations/using-github-enterprise-importer/completing-your-migration-with-github-enterprise-importer/accessing-your-migration-logs-for-github-enterprise-importer). */
+  /** The number of warnings encountered for this migration. To review the warnings, check the [Migration Log](https://docs.github.com/migrations/using-github-enterprise-importer/completing-your-migration-with-github-enterprise-importer/accessing-your-migration-logs-for-github-enterprise-importer). */
   warningsCount: Scalars['Int']['output'];
 };
 
@@ -21884,7 +21884,7 @@ export type RepositoryMigration = Migration & Node & {
   sourceUrl: Scalars['URI']['output'];
   /** The migration state. */
   state: MigrationState;
-  /** The number of warnings encountered for this migration. To review the warnings, check the [Migration Log](https://docs.github.com/en/migrations/using-github-enterprise-importer/completing-your-migration-with-github-enterprise-importer/accessing-your-migration-logs-for-github-enterprise-importer). */
+  /** The number of warnings encountered for this migration. To review the warnings, check the [Migration Log](https://docs.github.com/migrations/using-github-enterprise-importer/completing-your-migration-with-github-enterprise-importer/accessing-your-migration-logs-for-github-enterprise-importer). */
   warningsCount: Scalars['Int']['output'];
 };
 
