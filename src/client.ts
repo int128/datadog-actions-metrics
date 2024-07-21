65f4f363576b189b962ea0f29482c3a3f05462bb
Hidetake Iwata <int128@gmail.com>
2024-07-21 21:42:01 +0900

    Fix

--- src/client.ts
+++ src/client.ts
@@ -15,16 +15,14 @@ export type MetricsClient = {
   submitDistributionPoints(series: v1.DistributionPointsSeries[], description: string): Promise<void>
 }
 
+type MetricsFilter = <S extends v1.Series | v1.DistributionPointsSeries>(series: S[]) => S[]
+
 class DryRunMetricsClient implements MetricsClient {
-  constructor(
-    private readonly metricsFilter: (metric: string) => boolean,
-    private readonly tags: string[],
-  ) {}
+  constructor(private readonly metricsFilter: MetricsFilter) {}
 
   // eslint-disable-next-line @typescript-eslint/require-await
   async submitMetrics(series: v1.Series[], description: string): Promise<void> {
-    series = series.filter((s) => this.metricsFilter(s.metric))
-    series = injectTags(series, this.tags)
+    series = this.metricsFilter(series)
     core.startGroup(`Metrics payload (dry-run) (${description})`)
     core.info(JSON.stringify(series, undefined, 2))
     core.endGroup()
@@ -32,8 +30,7 @@ class DryRunMetricsClient implements MetricsClient {
 
   // eslint-disable-next-line @typescript-eslint/require-await
   async submitDistributionPoints(series: v1.DistributionPointsSeries[], description: string): Promise<void> {
-    series = series.filter((s) => this.metricsFilter(s.metric))
-    series = injectTags(series, this.tags)
+    series = this.metricsFilter(series)
     core.startGroup(`Distribution points payload (dry-run) (${description})`)
     core.info(JSON.stringify(series, undefined, 2))
     core.endGroup()
@@ -43,13 +40,11 @@ class DryRunMetricsClient implements MetricsClient {
 class RealMetricsClient implements MetricsClient {
   constructor(
     private readonly metricsApi: v1.MetricsApi,
-    private readonly metricsFilter: (metric: string) => boolean,
-    private readonly tags: string[],
+    private readonly metricsFilter: MetricsFilter,
   ) {}
 
   async submitMetrics(series: v1.Series[], description: string): Promise<void> {
-    series = series.filter((s) => this.metricsFilter(s.metric))
-    series = injectTags(series, this.tags)
+    series = this.metricsFilter(series)
     core.startGroup(`Metrics payload (${description})`)
     core.info(JSON.stringify(series, undefined, 2))
     core.endGroup()
@@ -59,8 +54,7 @@ class RealMetricsClient implements MetricsClient {
   }
 
   async submitDistributionPoints(series: v1.DistributionPointsSeries[], description: string): Promise<void> {
-    series = series.filter((s) => this.metricsFilter(s.metric))
-    series = injectTags(series, this.tags)
+    series = this.metricsFilter(series)
     core.startGroup(`Distribution points payload (${description})`)
     core.info(JSON.stringify(series, undefined, 2))
     core.endGroup()
@@ -70,7 +64,7 @@ class RealMetricsClient implements MetricsClient {
   }
 }
 
-const createFilter =
+export const createMatcher =
   (patterns: string[]) =>
   (metric: string): boolean => {
     let matched = false
@@ -91,9 +85,17 @@ export const injectTags = <S extends { tags?: string[] }>(series: S[], tags: str
   return series.map((s) => ({ ...s, tags: [...(s.tags ?? []), ...tags] }))
 }
 
+const createMetricsFilter = (inputs: Inputs): MetricsFilter => {
+  const matcher = createMatcher(inputs.metricsFilter)
+  return (series) => {
+    series = series.filter((s) => matcher(s.metric))
+    return injectTags(series, inputs.datadogTags)
+  }
+}
+
 export const createMetricsClient = (inputs: Inputs): MetricsClient => {
   if (inputs.datadogApiKey === undefined) {
-    return new DryRunMetricsClient(createFilter(inputs.metricsFilter), inputs.datadogTags)
+    return new DryRunMetricsClient(createMetricsFilter(inputs))
   }
 
   const configuration = client.createConfiguration({
@@ -107,7 +109,7 @@ export const createMetricsClient = (inputs: Inputs): MetricsClient => {
       site: inputs.datadogSite,
     })
   }
-  return new RealMetricsClient(new v1.MetricsApi(configuration), createFilter(inputs.metricsFilter), inputs.datadogTags)
+  return new RealMetricsClient(new v1.MetricsApi(configuration), createMetricsFilter(inputs))
 }
 
 const createHttpLibraryIfHttpsProxy = () => {
