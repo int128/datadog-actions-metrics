e8e799fd51b026e94ce73b579589f93121218840
Hidetake Iwata <int128@gmail.com>
2025-04-27 18:15:55 +0900

    Fix

--- tests/http.test.ts
+++ tests/http.test.ts
@@ -1,6 +1,4 @@
-import { test } from 'vitest'
-import { describe } from 'vitest'
-import { expect } from 'vitest'
+import { test, describe, expect, beforeAll, afterAll, beforeEach } from 'vitest'
 import { client, v1 } from '@datadog/datadog-api-client'
 import { createProxy } from 'proxy'
 import { HttpLibrary } from '../src/http.js'
